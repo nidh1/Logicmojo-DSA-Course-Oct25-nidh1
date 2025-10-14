e59380fbea18f3479db8783434278c94694c1d2e
nidh1 <87599245+nidh1@users.noreply.github.com>
2025-10-14 13:02:29 +0400

    Update searchInRotatedArray.java

--- assignmentAnswers/searchInRotatedArray.java
+++ assignmentAnswers/searchInRotatedArray.java
@@ -23,6 +23,29 @@ class Result {
 
     public static int search_element(List<Integer> arr, int target) {
     // Write your code here
+        int high = arr.size()-1;
+        int low = 0;
+        while(high>=low){
+            int mid = (high+low)/2;
+            if(arr.get(mid) == target){
+                return mid;
+            }
+            if(arr.get(mid) >= arr.get(low)){
+                if(arr.get(low) <= target && arr.get(mid) >= target){
+                    high = mid-1;
+                }else{
+                    low = mid+1;
+                }
+            }else{
+                if(arr.get(mid) <= target && arr.get(high) >= target){
+                    low = mid+1;
+                }else{
+                    high = mid-1;
+                }
+            }
+        }
+        
+        return -1;
 
     }
 
