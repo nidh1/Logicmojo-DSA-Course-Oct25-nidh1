4ca42312fc8c98d31a9c707f1be3394e9a55dfaf
nidh1 <87599245+nidh1@users.noreply.github.com>
2025-10-14 13:02:54 +0400

    Update singleElementInSortedArray.java

--- assignmentAnswers/singleElementInSortedArray.java
+++ assignmentAnswers/singleElementInSortedArray.java
@@ -23,6 +23,18 @@ class Result {
 
     public static int singlelement(int n, List<Integer> arr) {
     // Write your code here
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<arr.size(); i++){
+            map.put(arr.get(i), map.getOrDefault(arr.get(i), 0) + 1);
+        }
+        
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+            if(entry.getValue() == 1){
+                return entry.getKey();
+            }
+        }
+        
+        return -1;
 
     }
 
