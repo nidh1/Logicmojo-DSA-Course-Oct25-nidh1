80188e688dc5103e68fa37a1fd9e07fd1b6e3970
nidh1 <87599245+nidh1@users.noreply.github.com>
2025-11-06 22:38:41 +0400

    Add solution for longest common prefix problem

---
+++ classCodingAnswers/longestCommonPrefix.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        String s = "";
+        for(int i=0; i<strs[0].length(); i++){
+            if(strs[0].charAt(i) != strs[strs.length-1].charAt(i)){
+                break;
+            }
+            s+=strs[0].charAt(i);
+        }
+        return s;
+    }
+}
